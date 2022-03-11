4ebf0a08fa26e4fd9c362adfcd26167794a90235
Ana Paula Carvalho <anapcac@gmail.com>
2022-03-11 14:59:04 -0300

    Criação do Projeto Cadastro de Produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function App ( props ){
+  // O state guarda tudo que é variável dentro do componente e é utilizado para valores de variáveis que serão 
+  // modificados
+// state = {
+//   nome : ""
+// }
+
+// constructor(){
+//   super()
+//   this.modificarNome = this.modificarNome.bind(this)
+// }
+// modificarNome = (event) => {  
+//     nome: event.target.value  
+// }
+
+//Componente React
+const criaComboBox = () => {
+  const opcoes = ["Aninha","Carolzinha"]
+  const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <select>
+      {comboBoxOpcoes}
+    </select>  
+  )
+}
+const modificarNome = event => {
+  console.log(event.target.value)
+}
+//{this.criaComboBox()}
+  //render(){
+    // console.log('Entrei no Render')
+    //Toda vez que se quiar um componente é interessante coleçar o nome com letra Maiúscula
+    //Esta variável é uma Tag customizada
+    const MeuComboBox = () => criaComboBox()
+    return(
+    <>
+      <input className="text-centralizado" type="text"  value={props.nome} onChange={modificarNome} />
+      <h1>Hello {props.nome} sua idade é {props.idade}</h1>
+      <MeuComboBox/>
+    </>
+    )    
+ // }
 }
 
 export default App;
